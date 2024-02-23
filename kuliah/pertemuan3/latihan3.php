a5bfa4996f8e98410c3cbbf5bbb11483c74b3176
siwilmarr <wilmarardhana9@gmail.com>
2024-02-23 16:41:20 +0700

    tugas

--- kuliah/pertemuan3/latihan3.php
+++ kuliah/pertemuan3/latihan3.php
@@ -13,3 +13,22 @@ for ($nilai_awal = 10; $nilai_awal >= 1; $nilai_awal--) {
     echo "Hello Word! $nilai_awal <br>";
 }
 echo "selesai <br>";
+?>
+
+<?php
+$nama_depan = "paduka";
+$nama_belakang = "siwill";
+
+for ($i = 1; $i <= 100; $i++) {
+    echo "<br>";
+    if ($i % 2 == 0 && $i % 10 == 0) {
+        echo "$nama_depan $nama_belakang";
+    } else if ($i % 2 == 0) {
+        echo "$nama_depan";
+    } else if ($i % 10 == 0) {
+        echo "$nama_belakang";
+    } else {
+        echo "$i";
+    }
+}
+?>
